6ce86ff73c5045222ed13c58ae9918b97de0c29b
Maximilian Fischer <github@maaeps.de>
2022-05-30 02:55:36 +0200

    mainactivity

--- app/src/main/java/com/fischerscode/print/MainActivity.java
+++ app/src/main/java/com/fischerscode/print/MainActivity.java
@@ -18,13 +18,23 @@
 package com.fischerscode.print;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.githubIcon).setOnClickListener(v -> {
+            Uri uri = Uri.parse("https://github.com/fischerscode/AndroidPrint");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        });
     }
 }
